1cd0037b586c62af8691f9339971d3ef937eeec7
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2016-09-15 13:37:37 +0200

    Ajout de testNom

--- ExemplePHPObjet/tests/Entity/ContactTest.php
+++ ExemplePHPObjet/tests/Entity/ContactTest.php
@@ -7,11 +7,25 @@ use Smals\Entity\Contact;
 
 class ContactTest extends TestCase
 {
-    public function testPrenom() {
-        $contact = new Contact();
+    /**
+     * @var Contact
+     */
+    protected $contact;
 
-        $this->assertNull($contact->getPrenom());
-        $this->assertEquals($contact, $contact->setPrenom('Romain'));
-        $this->assertEquals('Romain', $contact->getPrenom());
+    protected function setUp()
+    {
+        $this->contact = new Contact();
+    }
+
+    public function testPrenom() {
+        $this->assertNull($this->contact->getPrenom());
+        $this->assertEquals($this->contact, $this->contact->setPrenom('Romain'));
+        $this->assertEquals('Romain', $this->contact->getPrenom());
+    }
+
+    public function testNom() {
+        $this->assertNull($this->contact->getNom());
+        $this->assertEquals($this->contact, $this->contact->setNom('Bohdanowicz'));
+        $this->assertEquals('Bohdanowicz', $this->contact->getNom());
     }
 }
